15f2d3eac5ccd09dd6910a56773ee06b2dca60da
Nick <75938990+NickSam55@users.noreply.github.com>
2024-06-29 19:53:18 +0100

    Novas alteracoes

---
+++ app/src/main/java/com/example/aplicativodepagamento/geracao_de_referencia.java
@@ -0,0 +1,35 @@
+package com.example.aplicativodepagamento;
+
+import android.os.Bundle;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class geracao_de_referencia extends AppCompatActivity {
+
+    private EditText editTextMatricula;
+    private EditText editTextCPF;
+    private EditText editTextCurso;
+    private Button buttonGerarReferencia;
+    private TextView textViewReferencia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_geracao_de_referencia);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
