a359a20ae198525d6e4431811291d68861c2a527
Nick <75938990+NickSam55@users.noreply.github.com>
2024-06-30 19:54:59 +0100

    Novas mudancas
    Escrevi os codigos para a pagina do menu, ou melhor, o menu. Um visual basico com 4 opcoes, pagamento, informacoes do aluno, historico de atividade e por ultimo configuracoes do aplicativo.

--- app/src/main/java/com/example/aplicativodepagamento/Menu_atividade.java
+++ app/src/main/java/com/example/aplicativodepagamento/Menu_atividade.java
@@ -1,10 +1,6 @@
 package com.example.aplicativodepagamento;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,24 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CriarConta extends AppCompatActivity {
-    TextView ja_tem_conta_password;
-    @SuppressLint("MissingInflatedId")
+public class Menu_atividade extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_criar_conta);
-        ja_tem_conta_password = findViewById(R.id.ja_tem_conta_password);
-
-
-        ja_tem_conta_password.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(CriarConta.this, ActividadePrincipal.class));
-            }
-        });
+        setContentView(R.layout.activity_menu_atividade);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
